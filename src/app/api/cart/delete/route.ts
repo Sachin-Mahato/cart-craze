a0b04fe323cd901c2961374d088c00795834f004
sachin <sachin333777@outlook.com>
2025-01-16 01:48:12 +0000

    fix: remove cart item

--- src/app/api/cart/delete/route.ts
+++ src/app/api/cart/delete/route.ts
@@ -1,27 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import { dbConnect } from "@/dbConfig/dbConnect";
 import Cart from "@/models/cartModel";
-import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
 
 export async function DELETE(req: NextRequest) {
-    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const id = new URL(req.url).searchParams.get("id");
+
+    if (!id) {
+        return NextResponse.json({ message: "ID not found" }, { status: 400 });
+    }
+
     try {
-        const { searchParams } = new URL(req.url);
-        const id = searchParams.get("id");
+        await dbConnect();
+        const deletedItem = await Cart.updateOne(
+            {
+                owner: session.user._id,
+            },
+            {
+                $pull: {
+                    items: {
+                        _id: id,
+                    },
+                },
+            }
+        );
 
-        if (!id) {
-            return NextResponse.json(
-                { message: "ID not found" },
-                { status: 400 }
-            );
-        }
-
-        const deleteItem = await Cart.deleteOne({
-            _id: new mongoose.Types.ObjectId(id),
-        });
-        console.log("Delete Result:", deleteItem);
-
-        if (deleteItem.deletedCount === 0) {
+        if (deletedItem.modifiedCount === 0) {
             return NextResponse.json(
                 { message: "Item not found or already deleted" },
                 { status: 404 }
@@ -33,12 +44,9 @@ export async function DELETE(req: NextRequest) {
             success: true,
         });
     } catch (error) {
-        // console.log(`error in delete: ${error}`);
-        if (error instanceof Error) {
-            console.error(`Error in DELETE /cart: ${error.message}`);
-        } else {
-            console.error(`Error in DELETE: ${error}`);
-        }
+        console.error(
+            `Error in DELETE /cart: ${error instanceof Error ? error.message : error}`
+        );
         return NextResponse.json(
             { message: "Internal Server Error", success: false },
             { status: 500 }
